00897957548dcba4c8e6fe857804990b288e020c
André Kallehauge <3846700+kallehauge@users.noreply.github.com>
2025-07-21 21:29:21 +0200

    Truck reversing sound: removed React.Profiler
    React Profiler's console.log output was interfering with Ink's terminal rendering in development mode, causing the "Cwd:" line to disappear during navigation.
    To be honest, then we didn't get any valuable information from the console.log since we do not have a dev console like we do in a browser; and we open up the app in an alternate buffer screen that doesn't display the log. We'll just rely on the React devtool instead.

--- src/views/app/App.tsx
+++ src/views/app/App.tsx
@@ -4,7 +4,7 @@ import { useGitRepository } from '@hooks/useGitRepository.js'
 import { useBranches } from '@hooks/useBranches.js'
 import { ThemeProvider } from '@contexts/ThemeProvider.js'
 import { ErrorView } from '@views/error/ErrorView.js'
-import React, { StrictMode } from 'react'
+import { StrictMode } from 'react'
 
 interface AppProps {
   includeRemote?: boolean
@@ -32,17 +32,6 @@ export function App(props: AppProps) {
     )
   }
 
-  const onRender = (
-    id: string,
-    phase: string,
-    actualDuration: number,
-    baseDuration: number,
-    startTime: number,
-    commitTime: number,
-  ) => {
-    console.log(id, phase, actualDuration, baseDuration, startTime, commitTime)
-  }
-
   const content = (
     <ThemeProvider theme={theme}>
       <AppProviders branches={branches}>
@@ -51,15 +40,5 @@ export function App(props: AppProps) {
     </ThemeProvider>
   )
 
-  return (
-    <StrictMode>
-      {process.env.DEV ? (
-        <React.Profiler id="app" onRender={onRender}>
-          {content}
-        </React.Profiler>
-      ) : (
-        content
-      )}
-    </StrictMode>
-  )
+  return <StrictMode>{content}</StrictMode>
 }
