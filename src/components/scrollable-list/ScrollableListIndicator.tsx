7d90c5722e47f42e0bda2aa95b4a22effbfd2a7f
André Kallehauge <3846700+kallehauge@users.noreply.github.com>
2025-08-05 19:11:04 +0200

    improvement(scrollable list): Reduce size of above/below indicator
    Showing two parentheses for situations where there are more both "above" and "below" didn't look very clean. The new version will display an arrow to indicate the direction; and hopefully that's enough visual que to signal what to do.

--- src/components/scrollable-list/ScrollableListIndicator.tsx
+++ src/components/scrollable-list/ScrollableListIndicator.tsx
@@ -15,12 +15,20 @@ export function ScrollableListIndicator({
   hasMoreBelow,
 }: ScrollableListIndicatorProps) {
   const { theme } = useAppUIContext()
+
+  let moreIndicator = ''
+  if (hasMoreAbove && hasMoreBelow) {
+    moreIndicator = ' (↑/↓)'
+  } else if (hasMoreAbove) {
+    moreIndicator = ' (↑)'
+  } else if (hasMoreBelow) {
+    moreIndicator = ' (↓)'
+  }
+
   return (
     <Box justifyContent="center" marginTop={1}>
       <Text color={theme.colors.selection}>
-        {currentIndex + 1} of {totalItems} items
-        {hasMoreAbove && ' (↑ more above)'}
-        {hasMoreBelow && ' (↓ more below)'}
+        {currentIndex + 1} of {totalItems} items{moreIndicator}
       </Text>
     </Box>
   )
